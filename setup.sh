8e981768a3d5ef36aecfada07ec7a8a41d563127
Laffy <3121560032@sv.sgu.edu.vn>
2024-10-05 18:05:06 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+REPO="https://github.com/Mirai3103/ftputil.git"
+# Clone the repository
+rm -rf temp
+git clone $REPO temp
+cd temp
+# Install required packages
+pip install -r requirements.txt
+rm -rf dist
+rm -rf build
+rm -rf main.spec
+python -m PyInstaller --onefile main.py
+sudo chmod +x ./dist/main
+sudo cp ./dist/main /usr/sbin/ftputil
+mv ./dist/main /usr/local/bin/ftputil
+rm -rf temp
